b973b288086447f9a52a8943cc2cccf9b5f04a40
sharron <10656035@ntub.edu.tw>
2021-05-14 16:33:36 +0800

    create

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path, include
+
+login_params = {
+    'template_name': 'users/login.html',
+    'redirect_authenticated_user':True,
+}
+
+
 
 urlpatterns = [
     path('coffees/', include('coffees.urls')),
     path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(),name='login'),
 ]
